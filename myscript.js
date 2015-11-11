c99bd15116924470df43721835eb4ed7e1d6dbd4
Martijniatus <mart45mart@hotmail.com>
2015-11-11 23:54:38 +0000

    Add three scripts to the repository

--- myscript.js
+++ myscript.js
@@ -1,5 +1,64 @@
-var img = document.getElementById('img');
+var angle = 0, img = document.getElementById('img');
+var urls = ["http://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Circle_-_black_simple.svg/220px-Circle_-_black_simple.svg.png",
+            "http://etc.usf.edu/clipart/40600/40694/pb_tri_40694_lg.gif"];
 var turn = function(){
-console.log("click registered");
-img.style.transform += "rotate(90deg)"; 
+        if (angle ===  0) {
+            console.log("1right");
+            angle = (angle+90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(90deg)";
+        }
+        else if (angle ===  90) {
+            console.log("2right");
+            angle = (angle+90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(90deg)";
+        }
+        else if (angle ===  180) {
+            console.log("3right");
+            angle = (angle+90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(90deg)";
+        } 
+        else { 
+            console.log("4right");
+            angle = (angle+90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(90deg)";
+        }
+};
+var turnleft = function(){
+        if (angle ===  0) {
+            console.log("1left");
+            angle = (angle+270)%360;
+            console.log(angle);
+            img.style.transform += "rotate(270deg)";
+        }
+        else if (angle ===  270) {
+            console.log("2left");
+            angle = (angle-90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(270deg)";
+        }
+        else if (angle ===  180) {
+            console.log("3left");
+            angle = (angle-90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(270deg)";
+        } 
+        else { 
+            console.log("4left");
+            angle = (angle-90)%360;
+            console.log(angle);
+            img.style.transform += "rotate(270deg)";
+        }
+};
+var guess = function(){
+        if (angle ===  0) {
+            alert("Good guess!");
+        }
+        else {
+            alert("Wrong guess dum dum!")
+        }
 };
