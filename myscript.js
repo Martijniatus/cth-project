78b9be85bce69558c823608ec1f48409dd00e129
Martijniatus <mart45mart@hotmail.com>
2015-11-12 00:46:55 +0000

    Button game!

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
 var angle = 0, img = document.getElementById('img');
+imgor = 0;
+imgo = "http://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg";
 var urls = ["http://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Circle_-_black_simple.svg/220px-Circle_-_black_simple.svg.png",
             "http://etc.usf.edu/clipart/40600/40694/pb_tri_40694_lg.gif"];
@@ -60,5 +62,26 @@ var guess = function(){
         }
         else {
             alert("Wrong guess dum dum!")
+        }
+};
+var nextimg = function(){
+    var image = document.getElementById('img');
+        if (imgor ===  0) {
+            console.log("nextimg1");
+            imgor = (imgor+1);
+            image.src = "http://etc.usf.edu/clipart/40600/40694/pb_tri_40694_lg.gif";
+            angle = (angle+270)%360;
+            img.style.transform += "rotate(270deg)";
+        }
+        else if (imgor ===  1) {
+            console.log("nextimg2");
+            imgor = (imgor+1);
+            image.src = "https://i.ytimg.com/vi/ReF6iQ7M5_A/maxresdefault.jpg";
+            img.style.transform += "rotate(180deg)";
+            angle = (angle+180)%360;
+        }
+        else{
+            image.src = "http://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg";
+            imgor = 0;
         }
 };
